2a68b6611dc95593dbac0812e8484fb110c8f543
Charif Abdallah <charifabdallah@iut-dhaka.edu>
2021-08-24 07:50:09 +0600

    Job commit

---
+++ JS/srcjs.js
@@ -0,0 +1,57 @@
+var x = document.getElementById("description1");
+var a = document.getElementById("description2");
+var b = document.getElementById("description3");
+var c = document.getElementById("description4");
+var d = document.getElementById("description5");
+var e = document.getElementById("description6");
+
+
+function MyFunction() {
+
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
+function MoreFunction1() {
+    if (a.style.display == "none") {
+        a.style.display = "block";
+    } else {
+        a.style.display = "none";
+    }
+}
+
+function MoreFunction2() {
+    if (b.style.display == "none") {
+        b.style.display = "block";
+    } else {
+        b.style.display = "none";
+    }
+}
+
+function MoreFunction3() {
+    if (c.style.display == "none") {
+        c.style.display = "block";
+    } else {
+        c.style.display = "none";
+    }
+}
+
+function MoreFunction4() {
+    if (d.style.display == "none") {
+        d.style.display = "block";
+    } else {
+        d.style.display = "none";
+    }
+}
+
+function MoreFunction5() {
+    if (e.style.display == "none") {
+        e.style.display = "block";
+    } else {
+        e.style.display = "none";
+    }
+}
